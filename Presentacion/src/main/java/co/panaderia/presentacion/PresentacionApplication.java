5cd1a7bd3f413b8f18f547bb0783aaf930641d23
DJSebastianArias <DJSebastianArias@192.168.1.77>
2019-07-21 18:09:57 -0500

    prueba subiendo

---
+++ Presentacion/src/main/java/co/panaderia/presentacion/PresentacionApplication.java
@@ -0,0 +1,13 @@
+package co.panaderia.presentacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentacionApplication.class, args);
+	}
+
+}
